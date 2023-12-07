b0459c91a07263549f64680d6e2880ee4861e9dd
HenryBautista <henrybautistahtt@hotmail.com>
2023-12-07 10:47:19 -0400

    Chor test

--- test/Excercise1.test.js
+++ test/Excercise1.test.js
@@ -1,6 +1,5 @@
 const excercise1 = require('../src/Excercises/Excercise1')
 
-
 test('It should wrap the gifts', () => {
     expect(excercise1(['cat', 'game', 'socks'])).toEqual([
         `*****\n*cat*\n*****`,

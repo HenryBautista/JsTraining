a8ba521789d3ef413a69a2a083227e8145474e87
HenryBautista <henrybautistahtt@hotmail.com>
2023-12-07 10:19:49 -0400

    Add Excercise 1

---
+++ src/Excercises/Excercise1.js
@@ -0,0 +1,15 @@
+function wrapping(gifts) {
+
+    const wrapped = gifts.map(gift => {
+        const wrapLength = gift.length + 2
+        const wraper = '*'.repeat(wrapLength)
+        const wrapedGift = `${wraper}\n*${gift}*\n${wraper}`
+        
+        return wrapedGift
+    });
+
+    return wrapped
+}
+
+
+console.log(wrapping(['cat', 'game', 'socks']))
